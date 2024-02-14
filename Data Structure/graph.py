2729bab97b853c2263c839031468f3188cd31e0b
mihirdeo16 <mihirdeo16@gmail.com>
2024-02-13 21:42:21 -0500

    Add type hint

--- Data Structure/graph.py
+++ Data Structure/graph.py
@@ -3,16 +3,19 @@
 Graph 
 """
 
-from typing import Dict
+from typing import Dict, List, Tuple
 from collections import defaultdict
 
 __author__ = "Mihir Deo"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
+edgeSet = List[Tuple[int, int]]
+adjacencyList = Dict[int, List[int]]
+adjacencyMatrix = List[List[int]]
 
 
-def edgeSet_to_adjacencyList(edge_set) -> Dict[int,list]:
+def edgeSet_to_adjacencyList(edge_set: edgeSet) -> adjacencyList:
 
     adjacency_list = defaultdict(list)
 
@@ -22,7 +25,7 @@ def edgeSet_to_adjacencyList(edge_set) -> Dict[int,list]:
     return adjacency_list
 
 
-def adjacencyList_to_edgeSet(adjacency_list) -> list[tuple]:
+def adjacencyList_to_edgeSet(adjacency_list: adjacencyList) -> edgeSet:
 
     edge_set = []
     for vertext, vertext_list in adjacency_list.items():
@@ -33,16 +36,17 @@ def adjacencyList_to_edgeSet(adjacency_list) -> list[tuple]:
     return sorted(list(edge_set))
 
 
-def edgeSet_to_adjacencyMatrix(edge_set) -> list[list[int]]:
+def edgeSet_to_adjacencyMatrix(edge_set: edgeSet) -> adjacencyMatrix:
 
-    size = max(max(edge_set)) +1
+    size = max(max(edge_set)) + 1
     matrix = [[0]*size for _ in range(size)]
 
     for edge_1, edge_2 in edge_set:
         matrix[edge_1][edge_2], matrix[edge_2][edge_1] = 1, 1
     return matrix
 
-def adjacencyList_to_adjacencyMatrix(adjacency_list:Dict[int,list]) -> list[list[int]]:
+
+def adjacencyList_to_adjacencyMatrix(adjacency_list: adjacencyList) -> adjacencyMatrix:
 
     size = len(adjacency_list.keys())
     matrix = [[0]*size for _ in adjacency_list.keys()]
@@ -53,40 +57,46 @@ def adjacencyList_to_adjacencyMatrix(adjacency_list:Dict[int,list]) -> list[list
     return matrix
 
 
-def adjacencyMatrix_to_adjacencyList(adjacency_matrix:list[list[int]]) -> Dict[int,list]:
+def adjacencyMatrix_to_adjacencyList(adjacency_matrix: adjacencyMatrix) -> adjacencyList:
 
     hashmap = defaultdict(list)
 
     for edge_1, edge_list in enumerate(adjacency_matrix):
         for index, edge_2 in enumerate(edge_list):
-            if edge_2==1:
+            if edge_2 == 1:
                 hashmap[edge_1].append(index)
     return hashmap
 
-def adjacencyMatrix_to_edgeSet(adjacency_matrix:list[list[int]]) -> list[tuple]:
+
+def adjacencyMatrix_to_edgeSet(adjacency_matrix: adjacencyMatrix) -> edgeSet:
 
     hashset = []
 
     for edge, edge_list in enumerate(adjacency_matrix):
 
         for edge_2, val in enumerate(edge_list):
-            if val ==1 and not any(set((edge,edge_2))== set(com) for com in hashset):
-                hashset.append((edge,edge_2))
+            if val == 1 and not any(set((edge, edge_2)) == set(com) for com in hashset):
+                hashset.append((edge, edge_2))
     return hashset
 
 
 def main():
     """ Main entry point of the app """
 
-    data = {(0, 1), (0, 2), (0, 3), (1, 3), (2, 3), (3, 4)}
+    data = [(0, 1), (0, 2), (0, 3), (1, 3), (2, 3), (3, 4)]
 
-    adjacency_list = edgeSet_to_adjacencyList(data)                           # HashSet to HashMap
-    adjacency_matrix = edgeSet_to_adjacencyMatrix(data)                       # Hashset to Matrix
-    edge_set = adjacencyList_to_edgeSet(adjacency_list)                       # HashMap to Hashset
-    adjacency_matrix = adjacencyList_to_adjacencyMatrix(adjacency_list)       # HashMap to Matrix    
-    adjacency_list = adjacencyMatrix_to_adjacencyList(adjacency_matrix)       # Matrix to HashMap
-    edge_set = adjacencyMatrix_to_edgeSet(adjacency_matrix)                   # Matrix to Hashset
-
+    adjacency_list = edgeSet_to_adjacencyList(
+        data)                           # HashSet to HashMap
+    adjacency_matrix = edgeSet_to_adjacencyMatrix(
+        data)                       # Hashset to Matrix
+    edge_set = adjacencyList_to_edgeSet(
+        adjacency_list)                       # HashMap to Hashset
+    adjacency_matrix = adjacencyList_to_adjacencyMatrix(
+        adjacency_list)       # HashMap to Matrix
+    adjacency_list = adjacencyMatrix_to_adjacencyList(
+        adjacency_matrix)       # Matrix to HashMap
+    edge_set = adjacencyMatrix_to_edgeSet(
+        adjacency_matrix)                   # Matrix to Hashset
 
 
 if __name__ == "__main__":
