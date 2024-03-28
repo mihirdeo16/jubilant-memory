3cfe61487467883f654d0a0dbb20ce09961b4ff4
mihirdeo16 <mihirdeo16@gmail.com>
2024-03-28 00:36:50 -0400

    Add graph algo

--- Algorithum/searching/graph_search.py
+++ Algorithum/searching/graph_search.py
@@ -11,10 +11,39 @@ from typing import Dict, Set, List
 
 adjacencyList = Dict[int,List[int]]
 
-def dfs(graph:adjacencyList)-> bool:
+def dfs(graph:adjacencyList):
 
+    visted = set()
+    traversal_order = []
 
-    return False
+    def dfs_util(node:int):
+        visted.add(node)
+        traversal_order.append(node)
+        for neighbor in graph[node]:
+            if neighbor not in visted:
+                dfs_util(neighbor)
+    
+    dfs_util(0)
+    print(traversal_order)
+
+def bfs(graph:adjacencyList):
+
+    visted = set()
+    traversal_order = []
+
+    def bfs_util(node:int):
+        visted.add(node)
+        queue = [node]
+        while queue:
+            node = queue.pop(0)
+            traversal_order.append(node)
+            for neighbor in graph[node]:
+                if neighbor not in visted:
+                    visted.add(neighbor)
+                    queue.append(neighbor)
+    
+    bfs_util(0)
+    print(traversal_order)
 
 def main():
     """ Main entry point of the app
@@ -26,16 +55,17 @@ def main():
     
     """
     graph = {
-         0: [1, 2],
-         1: [0, 2, 3, 4],
-         2: [0,2],
-         3: [1],
-         4: [1]
-         }
-
-    dfs(graph, 0)
-
-
+            0: [1, 2],
+            1: [2, 4],
+            2: [0 ],
+            3: [],
+            4: [3],
+        }
+    print(f"Graph: {graph}")
+    print(f"DFS: ")
+    dfs(graph)
+    print(f"BFS: ")
+    bfs(graph) 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
     main()
