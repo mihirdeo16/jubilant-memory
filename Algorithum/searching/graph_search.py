82a2d37eadd5c19a58d81138728d54e04c2da01e
mihirdeo16 <mihirdeo16@gmail.com>
2024-03-28 17:43:57 -0400

    Added info about graph logic

--- Algorithum/searching/graph_search.py
+++ Algorithum/searching/graph_search.py
@@ -12,6 +12,14 @@ from typing import Dict, Set, List
 adjacencyList = Dict[int,List[int]]
 
 def dfs(graph:adjacencyList):
+    """ Depth First Search
+    
+    Time Complexity: O(V+E) and Space Complexity: O(V)
+
+    It takes a graph as input and returns the traversal order of the graph using Depth First Search
+    Logic: We start with first node then visit its first neighbor and then its neighbor and so on. If we reach a node which has no neighbor to visit, we backtrack to the previous node and visit its other neighbor. We repeat this process until we visit all the nodes in the graph.
+    
+    """
 
     visted = set()
     traversal_order = []
@@ -24,9 +32,17 @@ def dfs(graph:adjacencyList):
                 dfs_util(neighbor)
     
     dfs_util(0)
-    print(traversal_order)
+    return traversal_order
 
 def bfs(graph:adjacencyList):
+    """ Breadth First Search
+    
+    Time Complexity: O(V+E) and Space Complexity: O(V)
+
+    It takes a graph as input and returns the traversal order of the graph using Breadth First Search
+
+    Logic: We start with the first node and visit all its neighbors.  Then we visit the neighbors of the neighbors at same level so on. We repeat this process until we visit all the nodes in the graph.
+    """
 
     visted = set()
     traversal_order = []
@@ -43,29 +59,27 @@ def bfs(graph:adjacencyList):
                     queue.append(neighbor)
     
     bfs_util(0)
-    print(traversal_order)
+    return traversal_order
 
 def main():
     """ Main entry point of the app
-          4 
-          |
-    0 ----1-----3
-    |     |
-    |-----2
-    
     """
+
+    # This is bi-directional graph
     graph = {
-            0: [1, 2],
-            1: [2, 4],
-            2: [0 ],
-            3: [],
-            4: [3],
+            0: [2, 1],
+            2: [0, 5, 6],
+            5: [2, 4],
+            4: [5],
+            6: [2],
+            1: [0, 10],
+            10: [1, 8],
+            8: [10, 9],
+            9: [8]
         }
-    print(f"Graph: {graph}")
-    print(f"DFS: ")
-    dfs(graph)
-    print(f"BFS: ")
-    bfs(graph) 
+    print(f"DFS: {dfs(graph)}")
+    print(f"BFS: {bfs(graph)}")
+
 if __name__ == "__main__":
     """ This is executed when run from the command line """
     main()
