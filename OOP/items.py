79d4bedd0e0a12567ac583bbd8b0c960a05d0b86
mihirdeo16 <mihirdeo16@gmail.com>
2024-11-04 21:48:21 -0500

    Learn property decorator

--- OOP/items.py
+++ OOP/items.py
@@ -15,17 +15,22 @@ class Staff:
         return f"Staff - 🧑 {self.name.upper()}, {self.age}, ${self.salary} 💸"
 
 class Employee:
-    def __init__(self,name:str,age:int,salary:float) -> None:
+    def __init__(self,first_name:str,last_name:str,age:int,salary:float) -> None:
         
         assert age >= 0, "Age should be positive integer"
         
-        self.name = name
+        self.first_name = first_name
+        self.last_name = last_name
         self.age = age
         self.salary = salary
-    
+
+    @property
+    def fullname(self) -> str:
+        return self.first_name + " " + self.last_name 
+
     def __repr__(self) -> str:
         
-        return f"Employee - 🧑 {self.name.upper()}, {self.age}, ${self.salary} 💸"
+        return f"Employee - 🧑 {self.first_name.upper()}, {self.age}, ${self.salary} 💸"
     
     def salary_hike(self,percent=10) -> str:
 
@@ -33,35 +38,34 @@ class Employee:
 
         self.salary = self.salary + self.salary*self.percent_scaled
 
-        return f"{self.name} new salary: {self.salary} 🤑"
+        return f"{self.first_name} new salary: {self.salary} 🤑"
     
     def __lt__(self,other) -> bool:
         return (self.salary < other.salary)
     
     def __eq__(self,other) -> bool:
 
-        return (self.name == other.name and self.age == other.age)
+        return (self.first_name == other.first_name and self.age == other.age)
 
     def __format__(self, __format_spec: str) -> str:
         if __format_spec == "f":
-            return str(f"{self.name} - {self.age} - {self.salary}").capitalize()
+            return str(f"{self.first_name} - {self.age} - {self.salary}").capitalize()
         
         return super().__format__(__format_spec)
         
 
 if __name__ == "__main__":
        
-    employee_obj_1 = Employee("John",25,8000)
-    employee_obj_2 = Employee("Kay",25,1000)
+    employee_obj_1 = Employee("John","Xie",25,8000)
+    employee_obj_2 = Employee("Kay","Smith",25,1000)
 
     print(employee_obj_1)
     print(employee_obj_1.salary_hike())
-
-    if employee_obj_1 == employee_obj_2:
-        print(f"Duplicate Entry")
+    print(employee_obj_1.fullname)
 
     sorted_staff_list = sorted([employee_obj_1,employee_obj_2])
     print(sorted_staff_list)
 
+    print('Data class Object:')
     staff_obj = Staff("John",25,8000)
     print(staff_obj)
