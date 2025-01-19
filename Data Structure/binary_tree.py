d08753783eb9e306d27938d67e48dff7eab732d9
mihirdeo16 <mihirdeo16@gmail.com>
2025-01-19 02:06:46 -0500

    Clean tree

--- Data Structure/binary_tree.py
+++ Data Structure/binary_tree.py
@@ -8,184 +8,95 @@ __version__ = "0.1.0"
 __license__ = "MIT"
 
 
-from typing import List, Any 
+import array
 import collections
+import queue
+from typing import List, Any
+
 
 class Node:
     """Tree Node """
 
-    def __init__(self, value=None, left=None, right=None) -> None:
-        self.value = value
+    def __init__(self, val=None, left=None, right=None) -> None:
+        self.val = val
         self.left = left
         self.right = right
 
     def __repr__(self) -> str:
-        return f"{self.value}"
+        return f"{self.val}"
 
 
-def depth_first_traversal_recursive(root):
+def dfs_binary_tree(root) -> list[Any]:
 
-    if root is None:
-        return []
-
-    return [root.value] + depth_first_traversal_recursive(root.left) + depth_first_traversal_recursive(root.right)
-
-
-def depth_first_traversal(root):
-
-    if root is None:
-        return []
-
-    traversal_stack = [root]
+    stack = [root]
     res = []
-    while traversal_stack:
-
-        node = traversal_stack.pop()
-        res.append(node.value)
+    while stack:
+        node = stack.pop()
+        res.append(node.val)
 
         if node.right:
-            traversal_stack.append(node.right)
+            stack.append(node.right)
+
         if node.left:
-            traversal_stack.append(node.left)
-
+            stack.append(node.left)
     return res
 
-def breath_first_traversal(root):
 
-    if root is None:
-        return []
+def bfs_binary_tree(root) -> list[Any]:
 
-    traversal_queue = collections.deque([root])
+    queue = collections.deque([root])
     res = []
-    while traversal_queue:
-
-        for _ in range(len(traversal_queue)):
-            node = traversal_queue.popleft()
-            res.append(node.value)
-
+    while queue:
+        local = []
+        for _ in range(len(queue)):
+            node = queue.popleft()
+            local.append(node.val)
             if node.left:
-                traversal_queue.append(node.left)
+                queue.append(node.left)
             if node.right:
-                traversal_queue.append(node.right)
-
+                queue.append(node.right)
+        res.append(local)
     return res
 
-
-def all_tree_sum(root):
-
-    if root is None:
-        return 0
-
-    return root.value + all_tree_sum(root.left) + all_tree_sum(root.right)
-
-def diameter_max_height_tree(root):
-
-    res = [0]
-    def height_tree(root):
-        if root is None:
-            return 0
-        
-        leftTree = max_height_tree(root.left)
-        rightTree = max_height_tree(root.right)
-
-        res[0] = max(res[0],1 + leftTree + rightTree )
-
-        return 1 + max(leftTree,rightTree)
-    height_tree(root)
-    return res[0]
-
-def max_height_tree(root):
-    if root is None:
-        return 0
-    
-    leftTree = max_height_tree(root.left)
-    rightTree = max_height_tree(root.right)
-
-    return 1 + max(leftTree,rightTree)
-
-def max_width_tree(root):
-    width = 0
-    queue = collections.deque([(root,0)])
-    while queue:
-        _, first_pos = queue[0]
-        _, last_pos = queue[-1]
-
-        for _ in range(len(queue)):
-            node, pos = queue.popleft()
-
-            if node.left:
-                queue.append((node.left,2*pos+1))
-            if node.right:
-                queue.append((node.left,2*pos+2))
-        width = max(width,last_pos-first_pos)
-    return width
-
-def pre_order_traversal(root):
-    """
-    N -> L -> R
-    """
-    if root is None:
-        return []]
-    return [root.val] + pre_order_traversal(root.left) + pre_order_traversal(root.right)
-
-def in_order_traversal(root):
-    """
-    L -> N -> R
-
-    """
-    if root is None:
-        return []
-    return in_order_traversal(root.left) + [root.val] + in_order_traversal(root.right)
-
-def post_order_traversal(root):
-    """
-    L -> R ->  N
-
-    """
-    if root is None:
-        return []
-    return post_order_traversal(root.left) + post_order_traversal(root.right)+ [root.val] 
 
 def array_representation(root) -> List[Any]:
     """
     Have to use Breath First Search
     """
-    queue = [root]
-    array= []
+    queue = collections.deque([(root,0)])
+    array = []
 
     while queue:
+        for _ in range(len(queue)):
 
-        node = queue.pop(0)
-        if node:
+            node, pos = queue.popleft()
 
-            array.append(node.value)
+            while len(array)<pos:
+                array.append(None)
+            array.append(node.val)
 
-            queue.append(node.left)
-            queue.append(node.right)
-
-        else:
-            array.append(None)
-    
-    while array and array[-1] is None:
-        array.pop()
+            if node.left:
+                queue.append((node.left,2*pos+1))
+            if node.right:
+                queue.append((node.right,2*pos+2))
 
     return array
+
 
 def breath_first_array(array_tree):
 
     if array_tree is None:
         return []
-    
-    queue= [0]
+
+    queue = [0]
     res = []
 
     while queue:
         current_index = queue.pop(0)
-        left_index = 2* current_index + 1
-        right_index = 2* current_index + 2
+        left_index = 2 * current_index + 1
+        right_index = 2 * current_index + 2
 
-
-        if current_index < len(array_tree) :
+        if current_index < len(array_tree):
             res.append(array_tree[current_index])
 
             if left_index < len(array_tree):
@@ -193,26 +104,27 @@ def breath_first_array(array_tree):
 
             if right_index < len(array_tree):
                 queue.append(right_index)
-            
+
     return res
+
 
 def depth_first_array(array_tree):
 
     if array_tree is None:
         return []
-    
-    stack= [0]
+
+    stack = [0]
     res = []
 
     while stack:
 
         current_index = stack.pop()
 
-        left_index = 2* current_index + 1
-        right_index = 2* current_index + 2
+        left_index = 2 * current_index + 1
+        right_index = 2 * current_index + 2
 
-        if current_index < len(array_tree) :
-        
+        if current_index < len(array_tree):
+
             res.append(array_tree[current_index])
 
             if right_index < len(array_tree):
@@ -223,12 +135,79 @@ def depth_first_array(array_tree):
 
     return res
 
-def depth_first_array_recursive(array_tree,index=0):
+
+def depth_first_array_recursive(array_tree, index=0):
     if array_tree is None or index >= len(array_tree):
         return []
     if len(array_tree) < 1:
         return array_tree[index]
-    return [array_tree[index]] + depth_first_array_recursive(array_tree,2*index+1) + depth_first_array_recursive(array_tree,2*index+2)
+    return [array_tree[index]] + depth_first_array_recursive(array_tree, 2*index+1) + depth_first_array_recursive(array_tree, 2*index+2)
+
+
+class BTIterator:
+    def __init__(self) -> None:
+        pass
+
+    def array_representation(self, root):
+
+        array = []
+        queue = collections.deque([(root, 0)])
+
+        while queue:
+            for _ in range(len(queue)):
+
+                node, pos = queue.popleft()
+
+                while len(array) < pos:
+                    array.append(None)
+                
+                if node.left:
+                    queue.append((root.left,2*pos+1))
+
+                if node.right:
+                    queue.append((root.right,2*pos+2))
+                array.append(node.val)
+
+        return array
+
+    def serialization(self, root):
+        array = array_representation(root)
+        array_str = [str(ele) if ele is not None else "Null" for ele in array]
+        str_array = "|".join(array_str)
+        return str_array
+
+    def deserialization(self, str_array):
+        array_str = str_array.split("|")
+        # array = [int(ele) if ele != "Null" else None for ele in array_str]
+        root = self.array_to_nodes_resp(array_str)
+        return root
+
+    def array_to_nodes_resp(self, array):
+
+        root = Node(val=array[0])
+        queue = collections.deque([(root, 0)])
+
+        while queue:
+            for _ in range(len(queue)):
+
+                node, pos = queue.popleft()
+
+                left_child_idx = 2*pos+1
+                right_child_idx = 2*pos+2
+
+
+                if left_child_idx < len(array) and array[left_child_idx]!="Null":
+
+                    node.left = Node(val=array[left_child_idx])
+                    queue.append((node.left, left_child_idx))
+
+                if right_child_idx < len(array) and array[right_child_idx]!="Null" :
+
+                    node.right = Node(val=array[right_child_idx])
+                    queue.append((node.right, right_child_idx))
+
+        return root
+    
 
 
 def main():
@@ -239,33 +218,44 @@ def main():
        11      4
       /  \       \
      5    2       1
+    /            /  \
+   8            7    14
 
     """
     root = Node(
-                3, 
-                left=Node(11, left=Node(4), right=Node(2)), 
-                right=Node(5, right=Node(1))
-                )
+        3,
+        left=Node(11,
+                  left=Node(5,
+                            left=Node(8)
+                            ),
+                  right=Node(2)
+                  ),
+        right=Node(4,
+                   right=Node(1,
+                              left=Node(7),
+                              right=Node(14)
+                              )
+                   )
+    )
 
-    print(f"Depth first traversal: {depth_first_traversal(root)}")
-    print(f"Depth first traversal recursively: {depth_first_traversal_recursive(root)}")
+    # Perform DFS using stack
+    res = dfs_binary_tree(root)
+    print(f"depth first traversal using stack: {res}")
 
-    print(f"Breath first traversal: {breath_first_traversal(root)}")
+    # Perform BFS using queue
+    res = bfs_binary_tree(root)
+    print(f"breath first traversal: {bfs_binary_tree(root)}")
 
-    print(f"sum of all tree nodes: {all_tree_sum(root)}")
-    # print(f"tree_min_value: {tree_min_value(root)}")
-    print(f"maximum depth of tree: {max_height_tree(root)}")
+    print(f"\narray representation: {array_representation(root)}")
 
-
-    print(f"\narray_representation: {array_representation(root)}")
-    array_tree = array_representation(root)
-
-    print(f"breath_first_array: {breath_first_array(array_tree)}")
-
-
-    print(f"depth_first_array: {depth_first_array(array_tree)}")
-    print(f"depth_first_array_recursive: {depth_first_array_recursive(array_tree)}")
-
+    # Tree serialization and deserialization using Class
+    print("\n")
+    binary_tree_obj = BTIterator()
+    serialize_tree = binary_tree_obj.serialization(root)
+    print(f"Serialize tree representation, BFS: {serialize_tree}")
+    deserialize_tree = binary_tree_obj.deserialization(serialize_tree)
+    res = bfs_binary_tree(deserialize_tree)
+    print(f"Deserialize tree traversal using BFS: {res}")
 
 
 if __name__ == "__main__":
