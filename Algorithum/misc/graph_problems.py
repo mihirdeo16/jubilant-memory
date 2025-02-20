8e25eef3502b35fcc32ab59c1927e8f9fe65c829
mihirdeo16 <mihirdeo16@gmail.com>
2025-02-19 21:50:52 -0500

    Added more problems

--- Algorithum/misc/graph_problems.py
+++ Algorithum/misc/graph_problems.py
@@ -7,7 +7,6 @@ __author__ = "Mihir Deo"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
-import re
 from typing import Dict, List
 import collections
 adjacencyList = Dict[int, List[int]]
@@ -115,18 +114,18 @@ def largest_connected_components(graph):
     vertex using DFS or BFS to find length of that connected component. Return that, and check if it max so far we 
     found and return that.
     """
-    if graph is None:
-        return 0
-    visited = set()
     max_count = 0
 
+    if graph is None:
+        return max_count
+    
+    visited = set()
     def dfs_utils(graph, vertex, visited):
-        if vertex not in visited:
-            visited.add(vertex)
-            count = 1
-            for neighbor in graph[vertex]:
-                if neighbor not in visited:
-                    count += dfs_utils(graph, neighbor, visited)
+        visited.add(vertex)
+        count = 1
+        for neighbor in graph[vertex]:
+            if neighbor not in visited:
+                count += dfs_utils(graph, neighbor, visited)
 
         return count
 
@@ -252,8 +251,33 @@ def findOrder(numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         
 
 
-    
+def cleanRoom(robot):
 
+    visited = set()
+    directions = [(0,1),(1,0),(0,-1),(-1,0)] # UP, RIGHT, DOWN, LEFT
+
+
+    def backtrack(x,y,curr_direction):
+        visited.add((x,y))
+        robot.clean()
+
+        for i in range(len(directions)):
+
+            new_direction = (curr_direction + i) % 4
+            new_x, new_y = x + directions[new_direction][0], y + directions[new_direction][1]
+
+            if (new_x, new_y) not in visited and robot.move():
+                backtrack(new_x,new_y,new_direction)
+                
+                # Reset the same spot
+                robot.turnLeft()
+                robot.turnLeft()
+                robot.move()
+                robot.turnLeft()
+                robot.turnLeft()
+        robot.turnLeft()
+
+    backtrack(x=0,y=0,curr_direction=0)
 
 
 def count_islands(grid):
@@ -435,6 +459,163 @@ def longestIncreasingPath(matrix: List[List[int]]) -> int:
 
     return max(visited.values())
 
+def unique_path(obstacleGrid):
+    if obstacleGrid[-1][-1]==1 or obstacleGrid[0][0]==1 :
+        return 0
+    if len(obstacleGrid)==1:
+        return 1 if 1 not in obstacleGrid[0] else 0
+    
+    memo = {}
+    def explore_path(x,y,obstacleGrid,memo):
+        rowBound, colBound = len(obstacleGrid), len(obstacleGrid[0])
+        # Out of bound issue
+        if x >= rowBound or y >= colBound:
+            return 0
+        
+        # We met the Obstacle
+        if obstacleGrid[x][y]==1:
+            return 0
+        
+        # We met the destination
+        if x == rowBound -1 and y == colBound -1:
+            return 1
+        
+        # Already visited
+        if (x,y) in memo:
+            return memo[(x,y)]
+        
+        # Explore 
+        right_dir_path = explore_path(x+1,y,obstacleGrid,memo)
+        down_dir_path = explore_path(x,y+1,obstacleGrid,memo)
+
+        memo[(x,y)] = right_dir_path + down_dir_path
+
+        return memo[(x,y)]
+    
+    explore_path(0,0,obstacleGrid,memo)
+
+    return memo[(0,0)]
+
+def calEquation(equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+    """
+    T - O(M * N), M- No. of queries, N - No. of equations
+    M - O(N) - No of equations
+    
+    """
+
+    # Create graph of adjacency list of weighted graph
+    graph = collections.defaultdict(dict)
+
+    for equa_var, val in zip(equations,values):
+        graph[equa_var[0]][equa_var[1]]= val
+        graph[equa_var[1]][equa_var[0]] = 1 /val 
+
+    def backtrack(src,dst,graph,acc,visited):
+        # If src is same as dst, return acc
+        if src ==dst:
+            return acc
+        
+        # Add node to visited set
+        visited.add(src)
+
+        # Explore
+        for neighbor, val in graph[src].items():
+            if neighbor not in visited:
+                result = backtrack(neighbor,dst,graph,acc*val,visited)
+                if result != -1.0:
+                    return result
+        return -1.0
+
+
+    res = []
+    for query in queries:
+        # Check if query[0] to query[1] is valid variables
+        src, dst = query[0], query[1]
+        if src not in graph or dst not in graph:
+            res.append(-1.0)
+        else:
+            # If they are valid, now calculate
+            visited = set()
+            acc = 1
+            acc = backtrack(src,dst,graph,acc,visited)
+            res.append(acc)
+    return res
+            
+        
+
+
+def find_good_farmland(grid:List[List[int]])-> float:
+    """
+    So in technical terms, we can call this as graph search problem, where we need to find out max square we can form.
+    We can slove this using backtracking, recursive call. Where we first define base conditions as backtrack on it.
+    We can treat this grid as coordinate space, adjacency matrix.
+    1) If we found 1, then its max_area = 1
+    2) To expand its right, down and diagonal should be 1.
+    3) Also we need to take care of boundaries.
+    4) To reduce husle of revisiting corrodinates we can use memo
+    
+    let say we have r - rows and c -cols then
+    T - O(r*c)
+    M - O(r*c)
+    """
+    def find_area(row,col,grid,visited):
+
+        rowBound, colBound = len(grid), len(grid[0])
+        if row >= rowBound or col >= colBound:
+            return 0
+
+        if grid[row][col]==0:
+            return 0
+
+        if (row,col) in visited:
+            return visited[(row,col)]
+
+        # Explore 
+        right_side = find_area(row+1,col,grid,visited) # Explore its right
+        down_side = find_area(row,col+1,grid,visited) # Explore its down
+        diagonal_side = find_area(row+1,col+1,grid,visited) # Explore its diagonal
+
+        visited[(row,col)] = min(right_side,diagonal_side,down_side) + 1
+
+        return visited[(row,col)]
+
+    visited = {}
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            find_area(row,col,grid,visited)
+    
+    return max(visited.values())
+
+
+def removeStones(stones: List[List[int]]) -> int:
+    
+    # Build graph
+    graph = collections.defaultdict(list)
+    for i in range(len(stones)):
+        for j in range(i+1, len(stones)):
+            if stones[i][0]==stones[j][0] or stones[i][1]==stones[j][1]:
+                graph[i].append(j)
+                graph[j].append(i)
+    
+    # Find connected components
+    def dfs(node,graph,visited):
+        visited.add(node)
+
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                dfs(neighbor,graph,visited)
+            
+    count = 0
+    visited = set()
+    for i in range(len(stones)):
+        if i not in visited:
+            dfs(i,graph,visited)
+            count += 1
+
+    return len(graph) - count
+
+
+
 def main():
     """ Main entry point of function.
                    0               11 --- 12        14 --- 15      16
@@ -519,6 +700,15 @@ def main():
     print(f"Find island with maximum size: {find_maximum_size_island(grid)}")
     print(f"Find island with minimum size: {find_minimum_size_island(grid)}")
 
+    grid = [
+            [0,1,1,0,1],
+            [1,1,0,1,0],
+            [0,1,1,1,0],
+            [1,1,1,1,0],
+            [1,1,1,1,1],
+            [0,0,0,0,0]
+        ]
+    print(f"Find maximum area of farming (square), {find_good_farmland(grid)}")
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
