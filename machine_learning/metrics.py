91ebf04601c84c58da102d8d9d54ff08f0a28b41
mihirdeo16 <mihirdeo16@gmail.com>
2025-11-14 21:10:30 -0500

    Added regression metrics

---
+++ machine_learning/metrics.py
@@ -0,0 +1,54 @@
+"""
+Metrics for evaluating machine learning models.
+
+- Regression Metrics: MAE, MSE, RMSE, MAPE
+- Classification Metrics: Accuracy, Precision, Recall, F1-Score
+- NLP Metrics: BLEU, ROUGE, Perplexity
+"""
+
+import numpy as np
+
+
+# Regression metrics
+
+def mean_absolute_error(a: np.ndarray, b: np.ndarray) -> np.float64:
+
+    return np.mean(np.abs(a - b), dtype=np.float64)
+
+
+def mean_squared_error(a: np.ndarray, b: np.ndarray) -> np.float64:
+
+    return np.mean(np.square(a - b), dtype=np.float64)
+
+
+def root_mean_squared_error(a: np.ndarray, b: np.ndarray) -> np.float64:
+
+    return np.sqrt(np.mean(np.square(a - b)), dtype=np.float64)
+
+
+def mean_abs_percentage_error(a: np.ndarray, b: np.ndarray) -> np.float64:
+
+    return np.mean(
+        np.multiply(np.divide(np.abs(a - b), a), 100), dtype=np.float64
+    )
+
+
+# Example usage:
+if __name__ == "__main__":
+    # Generate some sample data of 1d points
+    a = np.random.random(10)
+    b = np.random.random(10)
+
+    # Mean Absolute Error (MAE)
+    mae = mean_absolute_error(a, b)
+
+    # Mean Squared Error (MSE)
+    mse = mean_squared_error(a, b)
+
+    # Root Mean Squared Error (RMSE)
+    rmse = root_mean_squared_error(a, b)
+
+    # Mean Absolute Percentage Error (MAPE)
+    mape = mean_abs_percentage_error(a, b)
+
+    print(f"MAE: {mae}, MSE: {mse}, RMSE: {rmse}, MAPE: {mape}")
